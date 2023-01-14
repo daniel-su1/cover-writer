85049340e4b1282c8f67350f8223272a62940e45
daniel-su1 <107098486+daniel-su1@users.noreply.github.com>
2023-01-13 22:36:36 -0500

    created home page

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'))
 
-app.get('/', (req, res) => {
-    res.render('home')
+app.get('/', (req, res) => { 
+    res.render('home');
+})
+
+app.get('/start', (req, res) =>{
+    res.render('start');
 })
 
 app.listen(3000, () => {
-    console.log("LISTENING ON PORT 3000")
+    console.log("LISTENING ON PORT 3000");
 })
+
