f9ec929983fe51aa2d64926257888f363eabc8e9
daniel-su1 <107098486+daniel-su1@users.noreply.github.com>
2023-05-17 17:29:54 -0400

    update api key, change styles

--- public/js/app.js
+++ public/js/app.js
@@ -2,3 +2,10 @@ let learnMore = document.querySelector('#learnMore');
 learnMore.addEventListener('click', ()=>{
     document.querySelector('#scroll').scrollIntoView();
 })
+
+
+let submit = document.querySelector('#submit');
+submit.addEventListener('click', ()=>{
+    document.querySelector('#spinner').style.opacity = 1;
+})
+document.querySelector('#spinner').style.opacity = 1;
